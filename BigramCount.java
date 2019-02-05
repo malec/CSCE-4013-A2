66e595911bf47df0c268d660bfea5d0609cc0233
malec <alec@ahlbrandt.net>
2019-02-04 18:56:29 -0600

    bigramcount code added with tuples, not working

--- BigramCount.java
+++ BigramCount.java
@@ -10,21 +10,51 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import java.util.*;
 
 public class BigramCount {
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+	public static class Tuple<K,V> implements Comparable<Tuple>, Writable {
+		public K x;
+		public V y;
+
+		public Tuple() {
+
+		}
+
+		public Tuple(K x, V y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Tuple o) {
+			if (x.toString() == o.x.toString() && ((IntWritable)this.y).get() == ((IntWritable)o.y).get()) {
+				return 0;
+			}
+			else return -1;
+		}
+
+	}
+
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, Tuple<Text, IntWritable>> {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
+
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
+			if (itr.countTokens() != 1) {
+				Text previous = new Text(itr.nextToken());
+				while (itr.hasMoreTokens() && itr.countTokens() != 1) {
+					word.set(previous);
+					Text next = new Text(itr.nextToken());
+					context.write(previous, new Tuple(next, one));
+					previous = next;
+				}
 			}
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class IntSumReducer extends Reducer<Text, Tuple, Text, IntWritable> {
 		private IntWritable result = new IntWritable();
 
 		public void reduce(Text key, Iterable<IntWritable> values, Context context)
@@ -51,7 +81,7 @@ public class BigramCount {
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Tuple.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
